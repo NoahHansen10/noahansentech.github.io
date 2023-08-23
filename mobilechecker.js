35475922e1c82992de52e663e0b9d9a86e53a0fc
noah <ndhansen1031@gmail.com>
2023-08-23 00:52:35 -0400

    about fix

---
+++ mobilechecker.js
@@ -0,0 +1,19 @@
+ fetch('footer2.html')
+  .then(response => response.text())
+  .then(content => {
+    console.log(content); // Add this line to check the fetched content
+    footerContainer.innerHTML = content;
+  })
+  .catch(error => {
+    console.error('Error fetching footer:', error);
+  });
+
+    // Function to detect if the user is using a mobile device
+    function isMobileDevice() {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
+
+    // Redirect to the mobile subdomain if user is on a mobile device
+    if (isMobileDevice()) {
+      window.location.href = "https://dev.noahhansentech.tk/mobile"; // Replace with your mobile subdomain URL
+    }
---
+++ scripts.js
@@ -0,0 +1,30 @@
+const headerContainer = document.querySelector('header');
+const footerContainer = document.querySelector('footer');
+const navContainer = document.querySelector('nav');
+
+fetch('header2.html')
+  .then(response => response.text())
+  .then(content => {
+    headerContainer.innerHTML = content;
+  })
+  .catch(error => {
+    console.error('Error fetching header content:', error);
+  });
+
+fetch('navbar.html')
+  .then(response => response.text())
+  .then(content => {
+    navContainer.innerHTML = content;
+  })
+  .catch(error => {
+    console.error('Error fetching navbar:', error);
+  });
+
+fetch('footer.html')
+  .then(response => response.text())
+  .then(content => {
+    footerContainer.innerHTML = content;
+  })
+  .catch(error => {
+    console.error('Error fetching footer:', error);
+  });
