b09248a7b125119af180f76c8d7e30372df04a9b
noah <ndhansen1031@gmail.com>
2023-08-27 21:13:07 -0400

    changed Directoy updated html

--- postScripts.js
+++ postScripts.js
@@ -1,10 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const postList = document.getElementById('post-list');
 
-    // Replace 'posts/' with the correct path to your directory
     const postsDirectory = 'Posts/';
-
-    // List of post file names (without the .md extension)
     const postFileNames = [
         'post1',
         'post2',
@@ -12,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
         // Add more post file names as needed
     ];
 
-    // Populate the list with links to posts
     postFileNames.forEach(postFileName => {
         const postLink = document.createElement('a');
         postLink.href = `${postsDirectory}${postFileName}.md`;
